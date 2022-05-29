f76c5d9cbdac4dd7a874f453151da93ab992d90d
Olivier <olivier972@MacBook-Pro-de-Olivier.local>
2022-05-29 07:32:56 -0400

    try catch add to route publish and route to see all Room done

--- routes/room.js
+++ routes/room.js
@@ -6,38 +6,55 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 const router = express.Router();
 
 router.post("/room/publish", isAuthenticated, async (req, res) => {
-  // console.log(req.user);
-  // console.log(req.fields);
-  const { title, description, price, location } = req.fields;
-  // console.log(title);
-  // console.log(description);
-  // console.log(price);
-  // console.log(location);
-  const locationToArray = Object.values(location);
-  if (title && description && price && location) {
-    const newRoom = new Room({
-      title,
-      description,
-      location: locationToArray,
-      price,
-      user: req.user._id,
-    });
-    // console.log(newRoom);
+  try {
+    // console.log(req.user);
+    // console.log(req.fields);
+    const { title, description, price, location } = req.fields;
+    // console.log(title);
+    // console.log(description);
+    // console.log(price);
+    // console.log(location);
+    const locationToArray = Object.values(location);
+    if (title && description && price && location) {
+      const newRoom = new Room({
+        title,
+        description,
+        location: locationToArray,
+        price,
+        user: req.user._id,
+      });
+      // console.log(newRoom);
 
-    await newRoom.save();
-    res.json({
-      photos: [],
-      location: locationToArray,
-      _id: newRoom._id,
-      title: newRoom.title,
-      description: newRoom.description,
-      price: newRoom.price,
-      user: newRoom.user,
-      __v: newRoom.__v,
-    });
-  } else {
+      await newRoom.save();
+      res.json({
+        photos: [],
+        location: locationToArray,
+        _id: newRoom._id,
+        title: newRoom.title,
+        description: newRoom.description,
+        price: newRoom.price,
+        user: newRoom.user,
+        __v: newRoom.__v,
+      });
+    } else {
+      res.status(400).json({
+        message: "missing parameters",
+      });
+    }
+  } catch (error) {
     res.status(400).json({
-      message: "missing parameters",
+      message: error.message,
+    });
+  }
+});
+
+router.get("/rooms", async (req, res) => {
+  try {
+    const roomFind = await Room.find().select("-description");
+    res.json(roomFind);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
     });
   }
 });
